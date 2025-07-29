7adcc5a52883c4808617d804bb8b6801c168c90a
Humblewolfstudio <humblewolfstudio@gmail.com>
2025-07-29 19:44:09 +0900

    updated GetAppSupportDir to support macos, windows and linux

--- storage/disk.go
+++ storage/disk.go
@@ -7,17 +7,43 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
-// GetAppSupportDir returns the appropriate application support directory for macOS
+// GetAppSupportDir returns the appropriate application support directory based on the OS.
 func GetAppSupportDir() (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", fmt.Errorf("failed to get user home directory: %w", err)
 	}
-	//TODO add support for other systems
-	return filepath.Join(home, "Library", "Application Support", "mini-alt"), nil
+
+	var baseDir string
+	appName := "mini-alt"
+
+	switch runtime.GOOS {
+	case "darwin":
+		baseDir = filepath.Join(home, "Library", "Application Support", appName)
+
+	case "windows":
+		appData := os.Getenv("APPDATA")
+		if appData == "" {
+			return "", fmt.Errorf("APPDATA environment variable is not set")
+		}
+		baseDir = filepath.Join(appData, appName)
+
+	case "linux":
+		xdgConfig := os.Getenv("XDG_CONFIG_HOME")
+		if xdgConfig == "" {
+			xdgConfig = filepath.Join(home, ".config")
+		}
+		baseDir = filepath.Join(xdgConfig, appName)
+
+	default:
+		return "", fmt.Errorf("unsupported OS: %s", runtime.GOOS)
+	}
+
+	return baseDir, nil
 }
 
 // GetAppConfigDir returns the appropriate config directory for the application
