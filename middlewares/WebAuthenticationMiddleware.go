d44c159f460aee4ed64f47814f7986daa118e225
Humblewolfstudio <humblewolfstudio@gmail.com>
2025-08-10 14:52:44 +0900

    oops haha

--- middlewares/WebAuthenticationMiddleware.go
+++ middlewares/WebAuthenticationMiddleware.go
@@ -43,36 +43,30 @@ func WebAuthenticationAdminMiddleware(h *web.Handler) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id, err := c.Cookie("id")
 		if err != nil {
-			println("1")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		token, err := c.Cookie("token")
 		if err != nil {
-			println("2")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		admin, err := c.Cookie("admin")
 		if err != nil || admin != "true" {
-			println("3")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		idInt64, err := strconv.ParseInt(id, 10, 64)
 		if err != nil {
-			println("4")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		println(idInt64)
 		err = h.Store.AuthenticateAdmin(idInt64, token)
 		if err != nil {
-			println(err.Error())
-			println("5")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
